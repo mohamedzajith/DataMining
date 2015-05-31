9c8df0abac696cd9dc83129afb9b34d4d27973d8
MohamedZajith <mohamedzajith@gmail.com>
2015-05-31 22:19:39 +0530

    testing java code for git working or not

---
+++ src/com/mohamed/examples/testvcs.java
@@ -0,0 +1,10 @@
+package com.mohamed.examples;
+
+/**
+ * Created by dba on 5/31/15.
+ */
+public class testvcs {
+    public static void main(String[] args) {
+        System.out.println("testing git commit in intellij IDEA 14");
+    }
+}
